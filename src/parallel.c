307742ec096688f3c8a9f78099ca309d53805245
Daniel <daniel.amarante2@gmail.com>
2016-05-28 19:50:51 -0300

    Change spaces to tabs on parallel.c.

--- src/parallel.c
+++ src/parallel.c
@@ -4,45 +4,45 @@
 #define ARRAY_SIZE 40
 
 int *interleaving(int array[], int size) {
-    int *aux_array;
-    int i1, i2, i_aux;
+	int *aux_array;
+	int i1, i2, i_aux;
 
-    aux_array = calloc(size, sizeof(int));
+	aux_array = calloc(size, sizeof(int));
 
-    i1 = 0;
-    i2 = size / 2;
+	i1 = 0;
+	i2 = size / 2;
 
-    for (i_aux = 0; i_aux < size; i_aux++) {
-        if (((array[i1] <= array[i2]) && (i1 < (size / 2))) || (i2 == size))
-            aux_array[i_aux] = array[i1++];
-        else
-            aux_array[i_aux] = array[i2++];
-    }
+	for (i_aux = 0; i_aux < size; i_aux++) {
+		if (((array[i1] <= array[i2]) && (i1 < (size / 2))) || (i2 == size))
+			aux_array[i_aux] = array[i1++];
+		else
+			aux_array[i_aux] = array[i2++];
+	}
 
-    return aux_array;
+	return aux_array;
 }
 
 int main() {
-    int vetor[ARRAY_SIZE];
-    int *vetor2;
-    int i;
+	int vetor[ARRAY_SIZE];
+	int *vetor2;
+	int i;
 
-    for (i = 0; i < ARRAY_SIZE / 2; i++)
-        vetor[i] = i;
-    for (i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++)
-        vetor[i] = i - ARRAY_SIZE / 2;
+	for (i = 0; i < ARRAY_SIZE / 2; i++)
+		vetor[i] = i;
+	for (i = ARRAY_SIZE / 2; i < ARRAY_SIZE; i++)
+		vetor[i] = i - ARRAY_SIZE / 2;
 
-    printf("\nVetor: ");
-    for (i = 0; i < ARRAY_SIZE; i++)
-        printf("[%03d] ", vetor[i]);
+	printf("\nVetor: ");
+	for (i = 0; i < ARRAY_SIZE; i++)
+		printf("[%03d] ", vetor[i]);
 
-    vetor2 = interleaving(vetor, ARRAY_SIZE);
+	vetor2 = interleaving(vetor, ARRAY_SIZE);
 
 
-    printf("\nVetor: ");
-    for (i = 0; i < ARRAY_SIZE; i++)
-        printf("[%03d] ", vetor2[i]);
+	printf("\nVetor: ");
+	for (i = 0; i < ARRAY_SIZE; i++)
+		printf("[%03d] ", vetor2[i]);
 
-    return 0;
+	return 0;
 }
 
