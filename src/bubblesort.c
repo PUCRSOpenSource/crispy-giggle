b316d437233a3a5dce493189a252bc050522107e
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-05-28 19:45:38 -0300

    Updates bubblesort implementation

---
+++ src/bubblesort.c
@@ -0,0 +1,24 @@
+#include "bubblesort.h"
+
+void
+bubble_sort(int size, int* array)
+{
+	int c=0, holder, swap =1;
+	for (int i = 0; i < size-1 && swap; ++i)
+	{
+		swap = 0;
+		int limit = size - i -1;
+		for (int j = 0 ; j < limit; ++j)
+		{
+			int current = array[j];
+			int next = array[j+1];
+			if (current > next)
+			{
+				holder     = current;
+				array[j]   = next;
+				array[j+1] = holder;
+				swap       = 1;
+			}
+		}
+	}
+}
